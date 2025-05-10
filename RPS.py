89aa19d8dd1790d6f5667097921242da1521c953
asmae achahboun <asmae.achahboun@ump.ac.ma>
2025-05-10 17:05:00 +0000

    Improve RPS player strategy

--- RPS.py
+++ RPS.py
@@ -1,10 +1,18 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
 
 def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+    counter_moves = {'R': 'P', 'P': 'S', 'S': 'R'}
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    if prev_play:
+        opponent_history.append(prev_play)
 
-    return guess
+    if len(opponent_history) < 5:
+        return 'P'
+
+    counts = {'R': opponent_history.count('R'),
+              'P': opponent_history.count('P'),
+              'S': opponent_history.count('S')}
+
+    predicted_move = max(counts, key=counts.get)
+
+    return counter_moves[predicted_move]
